a944a987847212fa47add765525d3c1f8ef6acdc
njeririziki <rizikinjeri@gmail.com>
2025-04-24 13:53:14 +0300

    user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-
+// import { User } from '@prisma/client'; // Adjust the import based on your Prisma setup
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
